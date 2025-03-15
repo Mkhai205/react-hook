dccfdadead114f4055332c8760679369f0252b2e
MKhai205 <manhkhai123459876@gmail.com>
2025-03-15 14:32:46 +0700

    design sidebar admin-page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,23 @@
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+import img_bg from '../../assets/images/bg-sidebar.jpg';
+ 
+
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const [sidebarBg, setSidebarBg] = useState(img_bg);
+
     return (
-        <div>
-            <h1>Admin</h1>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed} sidebarBg={sidebarBg} />
+            </div>
+            <div className="admin-content">
+                <FaBars className="admin-toggle" onClick={() => setCollapsed(!collapsed)} />
+                content
+            </div>
         </div>
     );
 };
